289fe8333125bbb49179a11740d5ed56977e2d50
zjcnzxsjy <zjcnzxsjy@163.com>
2020-02-08 10:33:14 +0800

    组件use方式加载修改bug

--- src/index.js
+++ src/index.js
@@ -8,10 +8,22 @@ const VueFormDesigner = {
     formGenerator
 }
 
-
-Object.keys(VueFormDesigner).forEach(name => {
+// 为组件添加 install 方法，用于按需引入
+VueFormDesigner.install = function (Vue) {
+  Object.keys(VueFormDesigner).forEach(name => {
     Vue.component(name, VueFormDesigner[name]);
-});
+  });
+}
+
+const install = function (Vue) {
+  Object.keys(VueFormDesigner).forEach(name => {
+    Vue.component(name, VueFormDesigner[name]);
+  });
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
 
 export default VueFormDesigner;
 export { formDesigner, formGenerator };
